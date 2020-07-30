8aa29afa619d98d9ab9b872162fd15c65aeb1c8d
ANUBHAV JAIN <anubhavjain21799@gmail.com>
2020-07-31 04:34:46 +0530

    Deployed to firebase

---
+++ src/firebase/config.js
@@ -0,0 +1,10 @@
+export const config = {
+  apiKey: "AIzaSyByiXpVfbsgyuPj-gs1KFPrl_wYgLZxrxQ",
+  authDomain: "italiana-restaurant.firebaseapp.com",
+  databaseURL: "https://italiana-restaurant.firebaseio.com",
+  projectId: "italiana-restaurant",
+  storageBucket: "italiana-restaurant.appspot.com",
+  messagingSenderId: "839698708431",
+  appId: "1:839698708431:web:0ae5d359786362712704ba",
+  measurementId: "G-PYX28F7XTM"
+};
